3a0f204946f4f0891124682fb12b11c9a8b5060a
Bunnarith Heang <heangbunnarith@gmail.com>
2020-06-08 16:09:21 +0700

    color code

---
+++ constant.js
@@ -0,0 +1,14 @@
+var mainPinkColor = '#D40086';
+var mainDarkBgColor = '#333A40';
+
+var dealLabelBgColor = '#B75B22';
+var itemNameColor = '#3070BE';
+var priceLabelColor = '#FD3E3E';
+
+//Button
+var actionButtonBgColor = '#ECCA72';
+
+// Pagination Dots
+var activeDotColor = '#384FFF';
+var inActiveDotColor = '#707070';
+
