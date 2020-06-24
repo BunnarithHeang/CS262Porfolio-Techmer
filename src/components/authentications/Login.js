3c96ad38b836aabaeb5ae39a68c3703040150867
Vichea <mr.vichea.007@gmail.com>
2020-06-24 12:37:58 +0700

    a

--- src/components/authentications/Login.js
+++ src/components/authentications/Login.js
@@ -1,62 +1,37 @@
-import React, { Component } from "react";
-import {
-  FormGroup,
-  Label,
-  Input,
-  Button,
-  NavLink,
-  FormFeedback,
-  FormText,
-} from "reactstrap";
-import "../../scss/login.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component, useState } from "react";
+import axios from "axios";
+import { appUrl } from "./Helper";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = appUrl() + "/login";
+  }
+
+  componentDidMount() {
+    let data = {
+      username: "admin",
+      password: "admin",
+    };
+    let configs = {
+      headers: {},
+    };
+
+    // axios
+    //   .post(appUrl() + "/login", data, configs)
+    //   .then((response) => (this.message = response.accessToken));
+
+    axios
+      .get(appUrl() + "/role", configs)
+      .then((response, {}) => console.log(response));
+  }
+
   render() {
     return (
-      <div className="signin-container">
-        <div className="signin-form">
-          <div className="signin-logo-container">
-            <img
-              src="../../images/header_bg.jpg"
-              alt=""
-              className="signin-logo"
-            />
-          </div>
-          <FormGroup>
-            <Label for="exampleEmail">Username</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="username, email, or phone number "
-            />
-            <FormFeedback>You will not be able to see this</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleEmail">Password</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="password"
-              invalid
-            />
-            <FormFeedback>You will not be able to see this</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleEmail">Valid input</Label>
-            <Input valid />
-            <FormFeedback valid tooltip>
-              Sweet! that name is available
-            </FormFeedback>
-            <FormText>Example help text that remains unchanged.</FormText>
-          </FormGroup>
-          <Button color="primary" size="lg" block className="signin-button">
-            Sign In
-          </Button>
-          <NavLink className="signin-forgot"> forgot your password?</NavLink>
-        </div>
+      <div>
+        <h1>Hello</h1>
+        <h1>{this.state}</h1>
+        <h1>{this.message}</h1>
       </div>
     );
   }
