75287cd1d30708e05bab477107a69b290cdeb003
Vichea <mr.vichea.007@gmail.com>
2020-07-06 16:13:00 +0700

    a

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,141 +1,144 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 // import "../../scss/header.scss";
 
-class Header extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div>
-          {/* <!-- top Header --> */}
-
-          {/* <!-- header --> */}
-          <div id="header">
-            <div class="container">
-              <div class="pull-left">
-                {/* <!-- Logo --> */}
-                <div class="header-logo">
-                  <a class="logo" href="#">
-                    {/* <img src="./img/logo.png" alt="" /> */}
-                    techfinder
-                  </a>
-                </div>
-                {/* <!-- /Logo --> */}
-
-                {/* <!-- Search --> */}
-                <div class="header-search">
-                  <form>
-                    <input
-                      class="input search-input"
-                      type="text"
-                      placeholder="Enter your keyword"
-                    />
-                    <select class="input search-categories">
-                      <option value="0">All Categories</option>
-                      <option value="1">Category 01</option>
-                      <option value="1">Category 02</option>
-                    </select>
-                    <button class="search-btn">
-                      <i class="fa fa-search"></i>
-                    </button>
-                  </form>
-                </div>
-                {/* <!-- /Search --> */}
-              </div>
-
-              <div class="pull-right">
-                <ul class="header-btns">
-                  {/* <!-- Account --> */}
-                  <li class="header-account dropdown default-dropdown">
-                    <div
-                      class="dropdown-toggle"
-                      role="button"
-                      data-toggle="dropdown"
-                      aria-expanded="true"
-                    >
-                      <div class="header-btns-icon">
-                        <i class="fa fa-user-o"></i>
-                      </div>
-                      <strong class="text-uppercase">
-                        My Account <i class="fa fa-caret-down"></i>
-                      </strong>
-                    </div>
-                    <a href="#" class="text-uppercase">
-                      Login
-                    </a>{" "}
-                    /{" "}
-                    <a href="#" class="text-uppercase">
-                      Join
-                    </a>
-                    <ul class="custom-menu">
-                      <li>
-                        <Link to="/profile">
-                          <i class="fa fa-user-o"></i> My Account
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="#">
-                          <i class="fa fa-heart-o"></i> My Wishlist
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="#">
-                          <i class="fa fa-exchange"></i> Compare
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/checkout">
-                          <i class="fa fa-check"></i> Checkout
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/logout">
-                          <i class="fa fa-unlock-alt"></i> Logout
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="#">
-                          <i class="fa fa-user-plus"></i> Create An Account
-                        </Link>
-                      </li>
-                    </ul>
-                  </li>
-                  {/* <!-- /Account --> */}
-
-                  {/* <!-- Cart --> */}
-                  <li class="header-cart dropdown default-dropdown">
-                    <a
-                      class="dropdown-toggle"
-                      data-toggle="dropdown"
-                      aria-expanded="true"
-                    >
-                      <div class="header-btns-icon">
-                        <i class="fa fa-shopping-cart"></i>
-                        <span class="qty">3</span>
-                      </div>
-                      <strong class="text-uppercase">My Cart:</strong>
-                    </a>
-                  </li>
-                  {/* <!-- /Cart --> */}
-
-                  {/* <!-- Mobile nav toggle--> */}
-                  <li class="nav-toggle">
-                    <button class="nav-toggle-btn main-btn icon-btn">
-                      <i class="fa fa-bars"></i>
-                    </button>
-                  </li>
-                  {/* <!-- / Mobile nav toggle --> */}
-                </ul>
-              </div>
-            </div>
-            {/* <!-- header --> */}
-            <Navbar />
-          </div>
-        </div>
-      </React.Fragment>
-    );
+function Header() {
+  const [data, setData] = React.useState("");
+  const history = useHistory();
+  function clickHandler() {
+    // console.log(data);
+    window.location.reload(false);
+    history.push("/products/" + data.toSearch);
   }
+
+  return (
+    <React.Fragment>
+      <div>
+        {/* <!-- top Header --> */}
+
+        {/* <!-- header --> */}
+        <div id="header">
+          <div class="container">
+            <div class="pull-left">
+              {/* <!-- Logo --> */}
+              <div class="header-logo">
+                <a class="logo" href="#">
+                  {/* <img src="./img/logo.png" alt="" /> */}
+                  techfinder
+                </a>
+              </div>
+              {/* <!-- /Logo --> */}
+
+              {/* <!-- Search --> */}
+              <div class="header-search">
+                <form>
+                  <input
+                    class="input search-input"
+                    type="text"
+                    placeholder="Enter your keyword"
+                    onChange={(e) => setData({ toSearch: e.target.value })}
+                  />
+                  <button class="search-btn" onClick={() => clickHandler()}>
+                    <i class="fa fa-search"></i>
+                  </button>
+                </form>
+              </div>
+              {/* <!-- /Search --> */}
+            </div>
+
+            <div class="pull-right">
+              <ul class="header-btns">
+                {/* <!-- Account --> */}
+                <li class="header-account dropdown default-dropdown">
+                  <div
+                    class="dropdown-toggle"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-expanded="true"
+                  >
+                    <div class="header-btns-icon">
+                      <i class="fa fa-user-o"></i>
+                    </div>
+                    <strong class="text-uppercase">
+                      My Account <i class="fa fa-caret-down"></i>
+                    </strong>
+                  </div>
+                  <a href="#" class="text-uppercase">
+                    Login
+                  </a>{" "}
+                  /{" "}
+                  <a href="#" class="text-uppercase">
+                    Join
+                  </a>
+                  <ul class="custom-menu">
+                    <li>
+                      <Link to="/profile">
+                        <i class="fa fa-user-o"></i> My Account
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#">
+                        <i class="fa fa-heart-o"></i> My Wishlist
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#">
+                        <i class="fa fa-exchange"></i> Compare
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/checkout">
+                        <i class="fa fa-check"></i> Checkout
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/logout">
+                        <i class="fa fa-unlock-alt"></i> Logout
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#">
+                        <i class="fa fa-user-plus"></i> Create An Account
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                {/* <!-- /Account --> */}
+
+                {/* <!-- Cart --> */}
+                <li class="header-cart dropdown default-dropdown">
+                  <a
+                    class="dropdown-toggle"
+                    data-toggle="dropdown"
+                    aria-expanded="true"
+                  >
+                    <div class="header-btns-icon">
+                      <i class="fa fa-shopping-cart"></i>
+                      <span class="qty">3</span>
+                    </div>
+                    <strong class="text-uppercase">My Cart:</strong>
+                  </a>
+                </li>
+                {/* <!-- /Cart --> */}
+
+                {/* <!-- Mobile nav toggle--> */}
+                <li class="nav-toggle">
+                  <button class="nav-toggle-btn main-btn icon-btn">
+                    <i class="fa fa-bars"></i>
+                  </button>
+                </li>
+                {/* <!-- / Mobile nav toggle --> */}
+              </ul>
+            </div>
+          </div>
+          {/* <!-- header --> */}
+          <Navbar />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Header;
--- src/components/views/Home/Slider.js
+++ src/components/views/Home/Slider.js
@@ -1,15 +1,22 @@
-
-import SingleProduct from "./SingleProduct"
-import img1 from "../../../images/img-products/1.jpg"
+import SingleProduct from "./SingleProduct";
+import img1 from "../../../images/img-products/1.jpg";
 import React, { Component } from "react";
 import Slider from "react-slick";
+import Axios from "axios";
+import CategoryContainer from "../category/components/CategoryContainer";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block",backgroundColor:"black", borderRadius:"5px" , paddingTop:"2px"}}
+      style={{
+        ...style,
+        display: "block",
+        backgroundColor: "black",
+        borderRadius: "5px",
+        paddingTop: "2px",
+      }}
       onClick={onClick}
     />
   );
@@ -20,41 +27,56 @@ function SamplePrevArrow(props) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", backgroundColor: "black", borderRadius:"5px" , paddingTop:"2px"}}
+      style={{
+        ...style,
+        display: "block",
+        backgroundColor: "black",
+        borderRadius: "5px",
+        paddingTop: "2px",
+      }}
       onClick={onClick}
     />
   );
 }
 
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      prevArrow: <SamplePrevArrow />,
-      nextArrow: <SampleNextArrow />
-      
-    };
-    return (
-      <div>
-        <Slider {...settings}>
-            <div>
-            <SingleProduct badge="HOT" imgsrc={img1} discount="-20%" price="7$" after="5.6"/>
-            <SingleProduct badge="HOT" imgsrc={img1} discount="-30%" price="5$" after="3.5"/>
-            <SingleProduct badge="NEW" imgsrc={img1} discount="-10%" price="4$" after="3.6"/>
-            </div>
-            <div>
-            <SingleProduct badge="HOT" imgsrc={img1} discount="-20%" price="7$" after="5.6"/>
-            <SingleProduct badge="HOT" imgsrc={img1} discount="-30%" price="5$" after="3.5"/>
-            <SingleProduct badge="NEW" imgsrc={img1} discount="-10%" price="4$" after="3.6"/>
-            </div>
-            
-        </Slider>
-      </div>
-    );
-  }
+export default function SimpleSlider(props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
+  };
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    Axios.get("/home-page-product/" + props.title)
+      .then((res) => {
+        setProducts(
+          res.data.map((data, index) => (
+            <CategoryContainer product={data} key={index} />
+          ))
+        );
+      })
+      .catch((error) => console.log(error.response));
+  }, []);
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          {products[0]}
+          {products[1]}
+          {products[2]}
+        </div>
+        <div>
+          {products[3]}
+          {products[4]}
+          {products[5]}
+        </div>
+      </Slider>
+    </div>
+  );
 }
